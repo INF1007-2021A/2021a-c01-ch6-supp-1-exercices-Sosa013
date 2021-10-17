d484792bc5739269684fb4d5d0bd49c2e1f55eb6
Sosa013 <89996378+Sosa013@users.noreply.github.com>
2021-10-16 17:02:57 -0700

    done

--- exercice.py
+++ exercice.py
@@ -6,16 +6,28 @@ import math
 import copy
 import itertools
 
-
+# Question 1
 def get_maximums(numbers):
-	return [0]
+	list_initial = [numbers]
+	new_list = []
+	for numbers in list_initial:
+		for elem in numbers:
+			list_initial = new_list.append(max(elem))
 
+	return new_list
+
+# [max(elem) for elem in numbers] sa marche aussi bien
+
+# Question 2
 def join_integers(numbers):
-	return 0
 
+	return int("".join(([str(elem) for elem in numbers])))
+
+# Question 3
 def generate_prime_numbers(limit):
 	return [0]
 
+# Question 4
 def combine_strings_and_numbers(strings, num_combinations, excluded_multiples):
 	return [""]
 
